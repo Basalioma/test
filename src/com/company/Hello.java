1099cf9a8e2137268c1ce324e5796da7d42957b4
Даниил Кочагов <ya.teki-teki@yandex.ru>
2022-09-18 20:41:43 +0300

    Добавлена строчка А

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -3,9 +3,8 @@ package com.company;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("Hello, world!12");
-        System.out.println("2");
+        System.out.println("A");
+        System.out.println("Hello, world!");
     }
 
 }
